53ab444a1eba7e31a3901d53f6da9c3ee9091feb
MichaelHagler <67695612+MichaelHagler@users.noreply.github.com>
2022-12-06 06:41:18 -0800

    added urlencoded

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const express = require("express"),
 
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 //morgan to log all requests
