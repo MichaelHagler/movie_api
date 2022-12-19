32ac301403c88cbd498d0a7935976f1e12315d7c
MichaelHagler <67695612+MichaelHagler@users.noreply.github.com>
2022-12-18 18:39:29 -0800

    formatting

--- passport.js
+++ passport.js
@@ -7,36 +7,48 @@ let Users = Models.User,
   JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
 
-passport.use(new LocalStrategy({
-  usernameField: "username",
-  passwordField: "password"
-}, (username, password, callback) => {
-  console.log(username + " " + password);
-  Users.findOne({ username: username }, (error, user) => {
-    if (error) {
-      console.log(error);
-      return callback(error);
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "username",
+      passwordField: "password",
+    },
+    (username, password, callback) => {
+      console.log(username + " " + password);
+      Users.findOne({ username: username }, (error, user) => {
+        if (error) {
+          console.log(error);
+          return callback(error);
+        }
+
+        if (!user) {
+          console.log("incorrect username");
+          return callback(null, false, {
+            message: "Incorrect username or password.",
+          });
+        }
+
+        console.log("finsished");
+        return callback(null, user);
+      });
     }
+  )
+);
 
-    if (!user) {
-      console.log("incorrect username");
-      return callback(null, false, {message: "Incorrect username or password."});
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: "your_jwt_secret",
+    },
+    (jwtPayload, callback) => {
+      return Users.findById(jwtPayload._id)
+        .then((user) => {
+          return callback(null, user);
+        })
+        .catch((error) => {
+          return callback(error);
+        });
     }
-
-    console.log("finsished");
-    return callback(null, user);
-  });
-}));
-
-passport.use(new JWTStrategy({
-  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: "your_jwt_secret"
-}, (jwtPayload, callback) => {
-  return Users.findById(jwtPayload._id)
-    .then((user) => {
-      return callback(null, user);
-    })
-    .catch((error) => {
-      return callback(error)
-    });
-}));
+  )
+);
