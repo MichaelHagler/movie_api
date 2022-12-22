abca06ca6973d61d5c097b8fd6508c701bc85462
MichaelHagler <67695612+MichaelHagler@users.noreply.github.com>
2022-12-22 06:26:50 -0800

    installed cors and bcrypt. imported them in index.js and models.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express"),
   bodyParser = require("body-parser"),
   uuid = require("uuid");
 
+const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
 const mongoose = require("mongoose");
@@ -22,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(morgan("common"));
+app.use(cors());
 
 let auth = require("./auth")(app);
 
