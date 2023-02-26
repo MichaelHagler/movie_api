cfa67391088eaee3e10b66853896dbe6f5df4fa1
MichaelHagler <67695612+MichaelHagler@users.noreply.github.com>
2023-02-26 07:09:43 -0800

    Revert "reverting"
    This reverts commit f5d878046b61c94dff6409c65cb0b26a25822941.

--- index.js
+++ index.js
@@ -32,7 +32,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(morgan("common"));
 
-app.use(cors());
+let allowedOrigins = ["http://localhost:8080", "http://localhost:1234", "https://myflixmoviescf.netlify.app"];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = "The CORS policy for this application doesn't allow access from origin " + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 let auth = require("./auth")(app);
 
